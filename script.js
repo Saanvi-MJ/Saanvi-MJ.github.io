8c3e9162dfbf4ddaaf1916c2eeca4eabbd70f59a
saanvi-MJ <saanvijasmine@gmail.com>
2024-08-02 15:06:50 +0530

    feat: Added Confetti to the project

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-const hamburger = document.getElementById('hamburger'); 
-const menu = document.querySelector('.menu'); 
-  
-hamburger.addEventListener('click', function () { 
-    const hamIcon = this.querySelector('.hamburger-icon'); 
-    const crossIcon = this.querySelector('.cross-icon'); 
-    if (hamIcon.style.display === "none") { 
-        hamIcon.style.display = "inline-block"
-        menu.style.display = "none"
-        crossIcon.style.display = "none"
-    } 
-    else { 
-        crossIcon.style.display = "inline-block"
-        hamIcon.style.display = "none"
-        menu.style.display = "block"
-    } 
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', function () {
+  const hamIcon = this.querySelector('.hamburger-icon');
+  const crossIcon = this.querySelector('.cross-icon');
+  if (hamIcon.style.display === "none") {
+    hamIcon.style.display = "inline-block"
+    menu.style.display = "none"
+    crossIcon.style.display = "none"
+  }
+  else {
+    crossIcon.style.display = "inline-block"
+    hamIcon.style.display = "none"
+    menu.style.display = "block"
+  }
 });
- const commonBtn = document.querySelector(".btn common-btn");
-        commonBtn.addEventListener("click", ()=> {
-            confetti({
-                 particleCount: 100,
-                 spread: 90,
-                 origin: { y: 0.6 },
-                   });
-                   confetti({
-                     particleCount: 100,
-                     spread: 90,
-                     origin: { y: 0.6 },
-                   });
-                 });
-             
+const commonBtn = document.querySelector(".btn common-btn");
+commonBtn.addEventListener("click", () => {
+
+});
+
+function welcome() {
+  confetti({
+    particleCount: 100,
+    spread: 90,
+    origin: { x: 0, y: 0.5 },
+  });
+  confetti({
+    particleCount: 100,
+    spread: 90,
+    origin: { x: 1, y: 0.5 },
+  });
+}
